b14fd178e1850f987c3116f78c8ca95593558bb5
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2015-10-02 14:00:36 +0000

    Updated exception util.

--- Utils/src/ollie/utils/exception/ExceptionUtil.java
+++ Utils/src/ollie/utils/exception/ExceptionUtil.java
@@ -2,11 +2,25 @@ package ollie.utils.exception;
 
 public class ExceptionUtil {
 
-	public static String causeMessage(Exception e) {
-		String message = e.getMessage();
-		if (e.getCause() != null) {
-			message = e.getCause().getMessage();
+	/**
+	 * Gets the message from the root cause of the given throwable.
+	 * @param t - the throwable to find the cause message of.
+	 * @return the root cause message.
+	 */
+	public static String causeMessage(Throwable t) {
+		return cause(t).getMessage();
+	}
+	
+	/**
+	 * Gets the root cause of the given throwable.
+	 * @param t - the throwable to find the cause of.
+	 * @return the root cause.
+	 */
+	public static Throwable cause(Throwable t) {
+		Throwable result = t;
+		while (result.getCause() != null) {
+			result = result.getCause();
 		}
-		return message;
+		return result;
 	}
 }
