ef8f2bd1c79ceb85e73c68d80ac4b5bd1eeaf9a7
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2015-10-29 10:40:29 +0000

    Added seek method to buffer

--- Utils/src/ollie/utils/datastructure/ArrayByteBuffer.java
+++ Utils/src/ollie/utils/datastructure/ArrayByteBuffer.java
@@ -80,6 +80,17 @@ public class ArrayByteBuffer implements ByteBuffer {
 	}
 	
 	/* (non-Javadoc)
+     * @see ollie.utils.datastructure.ByteBuffer#seek(int)
+     */
+    @Override
+    public void seek(int pos) {
+        if (pos < 0 || pos > buffer.length) {
+            throw new IndexOutOfBoundsException("Position ["+pos+"] is outside the bounds of the buffer.");
+        }
+        readPos = pos;
+    }
+	
+	/* (non-Javadoc)
 	 * @see ollie.utils.datastructure.ByteBuffer#pos()
 	 */
 	@Override
