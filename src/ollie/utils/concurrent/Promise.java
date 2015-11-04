e3105ddbc6af41f9c43dd4ae812b1d7908960c41
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2015-11-04 15:57:09 +0000

    Added timeout exception to promise.

--- src/ollie/utils/concurrent/Promise.java
+++ src/ollie/utils/concurrent/Promise.java
@@ -1,7 +1,6 @@
 package ollie.utils.concurrent;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
