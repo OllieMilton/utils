138b95f303ae38fd8820a7b05f4d57c6e490266c
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2015-11-26 09:24:49 +0000

    Moved log entry code to utils project

---
+++ Utils/src/ollie/utils/logging/LogEntryAppender.java
@@ -0,0 +1,54 @@
+package ollie.utils.logging;
+
+import java.time.Instant;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+
+import ollie.utils.logging.LogEntry;
+
+public class LogEntryAppender extends AppenderSkeleton {
+
+	private LogEntryListener listener;
+	private String identifier;
+	
+	public LogEntryAppender(String identifier, LogEntryListener listener, Class<?>...classes) {
+		this.identifier = identifier;
+		this.listener = listener;
+		for (Class<?> c : classes) {
+			Logger.getLogger(c).addAppender(this);
+		}
+	}
+		
+	/* (non-Javadoc)
+	 * @see org.apache.log4j.Appender#close()
+	 */
+	@Override
+	public void close() {
+	
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.log4j.Appender#requiresLayout()
+	 */
+	@Override
+	public boolean requiresLayout() {
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.log4j.AppenderSkeleton#append(org.apache.log4j.spi.LoggingEvent)
+	 */
+	@Override
+	protected void append(LoggingEvent event) {
+		LogEntry entry = new LogEntry();
+		entry.setIdentifier(identifier);
+		entry.setTimeStamp(Instant.ofEpochMilli(event.getTimeStamp()));
+		entry.setLevel(event.getLevel().toString());
+		entry.setLoggingClass(event.getLoggerName());
+		entry.setMessage(event.getMessage().toString());
+		listener.onLogEntry(entry);
+	}
+	
+}
