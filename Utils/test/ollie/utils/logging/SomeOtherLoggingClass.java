0f88871a75c39b20c18276c766db47a38d60bc33
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2015-11-30 12:45:23 +0000

    Fixed bug with logger appender where duplicate messages got logged.

---
+++ Utils/test/ollie/utils/logging/SomeOtherLoggingClass.java
@@ -0,0 +1,9 @@
+package ollie.utils.logging;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class SomeOtherLoggingClass {
+
+	protected final Log logger = LogFactory.getLog("ollie.utils.logging");
+}
