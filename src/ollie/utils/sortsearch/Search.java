5670c370699c9efe98ad6fe0bf7eca9ec47f1403
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2016-02-26 09:10:49 +0000

    Added ability to pass a comparator into binary search function.

--- src/ollie/utils/sortsearch/Search.java
+++ src/ollie/utils/sortsearch/Search.java
@@ -1,6 +1,7 @@
 package ollie.utils.sortsearch;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Search {
 
@@ -12,6 +13,22 @@ public class Search {
 				return binarySearch(Arrays.copyOfRange(arr, 0, mid), x);
 			} else if (cmp > 0) {
 				return binarySearch(Arrays.copyOfRange(arr, mid, arr.length), x);
+			} else {
+				return arr[mid].equals(x) ? arr[mid] : null;
+			}
+		} else {
+			return arr[0].equals(x) ? arr[0] : null;
+		}
+	}
+	
+	public static <T> T binarySearch(T[] arr, T x, Comparator<T> comparator) {
+		if (arr.length > 1) {
+			int mid = arr.length / 2;
+			int cmp = comparator.compare(x, arr[mid]);
+			if (cmp < 0) {
+				return binarySearch(Arrays.copyOfRange(arr, 0, mid), x, comparator);
+			} else if (cmp > 0) {
+				return binarySearch(Arrays.copyOfRange(arr, mid, arr.length), x, comparator);
 			} else {
 				return arr[mid].equals(x) ? arr[mid] : null;
 			}
