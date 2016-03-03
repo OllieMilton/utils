46e39de47db742cbc0ee2f2f84bda9242a446a41
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2016-03-03 14:12:08 +0000

    Now uses log4j2

--- src/ollie/utils/logging/l4j/LogEntryListener.java
+++ src/ollie/utils/logging/l4j/LogEntryListener.java
@@ -2,6 +2,7 @@ package ollie.utils.logging.l4j;
 
 import ollie.utils.logging.l4j.LogEntry;
 
+@FunctionalInterface
 public interface LogEntryListener {
 
 	void onLogEntry(LogEntry entry);
--- test/ollie/utils/logging/l4j/LogEntryAppenderTest.java
+++ test/ollie/utils/logging/l4j/LogEntryAppenderTest.java
@@ -4,8 +4,6 @@ import java.util.LinkedList;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,14 +13,14 @@ import ollie.utils.logging.SomeOtherLoggingClass;
 
 @RunWith(JUnit4.class)
 public class LogEntryAppenderTest {
-
+	
 	private Log logger = LogFactory.getLog(getClass());
 	
 	@Test
 	public void testAppender() {
 		SomeShitClass shit = new SomeShitClass();
 		LinkedList<LogEntry> logEntries = new LinkedList<>();
-		new LogEntryAppender("LogEntryAppenderTest", (entry) -> logEntries.add(entry), getClass(), shit.getClass());
+		LogEntryAppender.registerAppender("LogEntryAppenderTest-1", (entry) -> logEntries.add(entry));
 		logger.debug("starting shit logger...");
 		shit.log();
 		
@@ -36,8 +34,8 @@ public class LogEntryAppenderTest {
 		Assert.assertEquals("starting shit logger...", logEntries.get(0).getMessage());
 		Assert.assertEquals("shit", logEntries.get(1).getMessage());
 		
-		Assert.assertEquals("LogEntryAppenderTest", logEntries.get(0).getIdentifier());
-		Assert.assertEquals("LogEntryAppenderTest", logEntries.get(1).getIdentifier());
+		Assert.assertEquals("LogEntryAppenderTest-1", logEntries.get(0).getIdentifier());
+		Assert.assertEquals("LogEntryAppenderTest-1", logEntries.get(1).getIdentifier());
 		
 	}
 	
@@ -47,7 +45,7 @@ public class LogEntryAppenderTest {
 		@SuppressWarnings("unused")
 		SomeOtherLoggingClass other = new SomeOtherLoggingClass();
 		LinkedList<LogEntry> logEntries = new LinkedList<>();
-		new LogEntryAppender("LogEntryAppenderTest", (entry) -> logEntries.add(entry));
+		LogEntryAppender.registerAppender("LogEntryAppenderTest-2", (entry) -> logEntries.add(entry));
 		logger.debug("starting shit logger...");
 		shit.log();
 		
@@ -61,8 +59,8 @@ public class LogEntryAppenderTest {
 		Assert.assertEquals("starting shit logger...", logEntries.get(0).getMessage());
 		Assert.assertEquals("shit", logEntries.get(1).getMessage());
 		
-		Assert.assertEquals("LogEntryAppenderTest", logEntries.get(0).getIdentifier());
-		Assert.assertEquals("LogEntryAppenderTest", logEntries.get(1).getIdentifier());
+		Assert.assertEquals("LogEntryAppenderTest-2", logEntries.get(0).getIdentifier());
+		Assert.assertEquals("LogEntryAppenderTest-2", logEntries.get(1).getIdentifier());
 		
 	}
 	
@@ -70,9 +68,9 @@ public class LogEntryAppenderTest {
 	public void testSetLogLevel() {
 		SomeShitClass shit = new SomeShitClass();
 		SomeOtherLoggingClass other = new SomeOtherLoggingClass();
-		Logger.getRootLogger().setLevel(Level.TRACE);
+		//Logger.getRootLogger().setLevel(Level.TRACE);
 		LinkedList<LogEntry> logEntries = new LinkedList<>();
-		new LogEntryAppender("LogEntryAppenderTest", (entry) -> logEntries.add(entry));
+		LogEntryAppender.registerAppender("LogEntryAppenderTest-3", (entry) -> logEntries.add(entry));
 		logger.debug("starting shit logger...");
 		shit.log();
 		other.logTrace();
@@ -89,12 +87,13 @@ public class LogEntryAppenderTest {
 		Assert.assertEquals("shit", logEntries.get(1).getMessage());
 		Assert.assertEquals("A trace message from some other class.", logEntries.get(2).getMessage());
 		
-		Assert.assertEquals("LogEntryAppenderTest", logEntries.get(0).getIdentifier());
-		Assert.assertEquals("LogEntryAppenderTest", logEntries.get(1).getIdentifier());
-		Assert.assertEquals("LogEntryAppenderTest", logEntries.get(2).getIdentifier());
+		Assert.assertEquals("LogEntryAppenderTest-3", logEntries.get(0).getIdentifier());
+		Assert.assertEquals("LogEntryAppenderTest-3", logEntries.get(1).getIdentifier());
+		Assert.assertEquals("LogEntryAppenderTest-3", logEntries.get(2).getIdentifier());
 	}
 	
 	private class SomeShitClass {
+		
 		private Log logger = LogFactory.getLog(getClass());
 		
 		public void log() {
