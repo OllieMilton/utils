6a4a39b54c8219d2cb58ee2c39ba99052802c3a9
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2016-03-17 13:18:16 +0000

    Added JSON builder class

---
+++ src/ollie/utils/serialisation/JSONBuilder.java
@@ -0,0 +1,160 @@
+package ollie.utils.serialisation;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import ollie.utils.Strings;
+
+public class JSONBuilder {
+
+	private JSONObject root;
+	private JSONObject currentObject;
+	private int openObjects;
+	
+	private JSONBuilder() {
+		root = new JSONObject(null);
+		currentObject = root;
+		openObjects = 0;
+	}
+	
+	public static JSONBuilder newJSONBuilder() {
+		return new JSONBuilder();
+	}
+	
+	public JSONBuilder startObject(String name) {
+		if (Strings.isBlank(name)) {
+			throw new NullPointerException("Object must have a name.");
+		}
+		JSONObject newObj = new JSONObject(name);
+		if (currentObject.nodes.contains(newObj)) {
+			throw new IllegalArgumentException("Duplicate field name.");
+		}
+		newObj.parent = currentObject;
+		currentObject.nodes.add(newObj);
+		currentObject = newObj;
+		openObjects ++;
+		return this;
+	}
+	
+	public JSONBuilder endObject() {
+		if (currentObject != root) {
+			openObjects --;
+			currentObject = currentObject.parent;
+		}
+		return this;
+	}
+	
+	public JSONBuilder field(String name, Object value) {
+		if (Strings.isBlank(name)) {
+			throw new NullPointerException("Field must have a name.");
+		}
+		JSONNode node;
+		if (value instanceof Number || value instanceof Boolean) {
+			node = new JSONPrimativeField(name, String.valueOf(value));
+		} else {
+			node = new JSONStringField(name, String.valueOf(value));	
+		}
+		if (currentObject.nodes.contains(node)) {
+			throw new IllegalArgumentException("Duplicate field name.");
+		}
+		currentObject.nodes.add(node);
+		return this;
+	}
+	
+	@Override
+	public String toString() {
+		if (openObjects > 0) {
+			throw new IllegalStateException("One or more objects not closed.");
+		}
+		return root.toString();
+	}
+	
+	private class JSONNode {
+		String name;
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (!(obj instanceof JSONNode))
+				return false;
+			JSONNode other = (JSONNode) obj;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			return true;
+		}
+
+	}
+	
+	private class JSONObject extends JSONNode {
+		JSONObject parent;
+		List<JSONNode> nodes = new ArrayList<>();
+		
+		JSONObject(String name) {
+			this.name = name;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder result;
+			if (Strings.isNotBlank(name)) {
+				result = new StringBuilder("'"+name+"':{");
+			} else {
+				result = new StringBuilder("{");
+			}
+			for (Iterator<JSONNode> itr = nodes.iterator(); itr.hasNext();) {
+				result.append(itr.next().toString());
+				if (itr.hasNext()) {
+					result.append(",");
+				}
+			}
+			result.append("}");
+			return result.toString();
+		}
+
+	}
+	
+	private class JSONStringField extends JSONNode {
+		String value;
+		
+		JSONStringField(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
+		
+		@Override
+		public String toString() {
+			return "'"+name+"':'"+value+"'";
+		}
+		
+	}
+	
+	private class JSONPrimativeField extends JSONNode {
+		String value;
+		
+		JSONPrimativeField(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
+		
+		@Override
+		public String toString() {
+			return "'"+name+"':"+value;
+		}
+		
+	}
+}
---
+++ test/ollie/utils/serialisation/JSONBuilderTest.java
@@ -0,0 +1,103 @@
+package ollie.utils.serialisation;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class JSONBuilderTest {
+
+	@Test
+	public void simpleJSON() {
+		Assert.assertEquals("{'stringTest':'string'}", JSONBuilder.newJSONBuilder().field("stringTest", "string").toString());
+		Assert.assertEquals("{'booleanTest':true}", JSONBuilder.newJSONBuilder().field("booleanTest", true).toString());
+		Assert.assertEquals("{'primNumberTest':666}", JSONBuilder.newJSONBuilder().field("primNumberTest", 666L).toString());
+		Assert.assertEquals("{'objNumberTest':66.7}", JSONBuilder.newJSONBuilder().field("objNumberTest", new Double(66.7)).toString());
+	}
+
+	@Test
+	public void simpleJSONMultiFields() {
+		Assert.assertEquals("{'stringTest':'string','booleanTest':true,'primNumberTest':666,'objNumberTest':66.7}", 
+				JSONBuilder.newJSONBuilder()
+				.field("stringTest", "string")
+				.field("booleanTest", true)
+				.field("primNumberTest", 666L)
+				.field("objNumberTest", new Double(66.7))
+				.toString());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void simpleJSONMultiFieldsWithError() {
+		Assert.assertEquals("{'stringTest':'string','stringTest':true,'primNumberTest':666,'objNumberTest':66.7}", 
+				JSONBuilder.newJSONBuilder()
+				.field("stringTest", "string")
+				.field("stringTest", true)
+				.field("primNumberTest", 666L)
+				.field("objNumberTest", new Double(66.7))
+				.toString());
+	}
+	
+	@Test(expected=IllegalStateException.class)
+	public void complexJSONWithError() {
+		Assert.assertEquals("{'inner':{'stringTest':'string','booleanTest':true,'primNumberTest':666,'objNumberTest':66.7}}", 
+				JSONBuilder.newJSONBuilder()
+				.startObject("inner")
+				.field("stringTest", "string")
+				.field("booleanTest", true)
+				.field("primNumberTest", 666L)
+				.field("objNumberTest", new Double(66.7))
+				.toString());
+	}
+	
+	@Test
+	public void complexJSON() {
+		Assert.assertEquals("{'inner':{'stringTest':'string','booleanTest':true,'primNumberTest':666,'objNumberTest':66.7}}", 
+				JSONBuilder.newJSONBuilder()
+				.startObject("inner")
+				.field("stringTest", "string")
+				.field("booleanTest", true)
+				.field("primNumberTest", 666L)
+				.field("objNumberTest", new Double(66.7))
+				.endObject()
+				.toString());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void complexJSONNestedWithError() {
+		Assert.assertEquals("{'inner':{'stringTest':'string','booleanTest':true,'primNumberTest':666,'objNumberTest':66.7},'inner':{'stringTest':'string','booleanTest':true,'primNumberTest':666,'objNumberTest':66.7}}", 
+				JSONBuilder.newJSONBuilder()
+				.startObject("inner")
+				.field("stringTest", "string")
+				.field("booleanTest", true)
+				.field("primNumberTest", 666L)
+				.field("objNumberTest", new Double(66.7))
+				.endObject()
+				.startObject("inner")
+				.field("stringTest", "string")
+				.field("booleanTest", true)
+				.field("primNumberTest", 666L)
+				.field("objNumberTest", new Double(66.7))
+				.endObject()
+				.toString());
+	}
+	
+	@Test
+	public void complexJSONNested() {
+		Assert.assertEquals("{'inner':{'stringTest':'string','booleanTest':true,'primNumberTest':666,'objNumberTest':66.7},'inner2':{'stringTest':'string','booleanTest':true,'primNumberTest':666,'objNumberTest':66.7}}", 
+				JSONBuilder.newJSONBuilder()
+				.startObject("inner")
+				.field("stringTest", "string")
+				.field("booleanTest", true)
+				.field("primNumberTest", 666L)
+				.field("objNumberTest", new Double(66.7))
+				.endObject()
+				.startObject("inner2")
+				.field("stringTest", "string")
+				.field("booleanTest", true)
+				.field("primNumberTest", 666L)
+				.field("objNumberTest", new Double(66.7))
+				.endObject()
+				.toString());
+	}
+}
