e42620a2f0404528f80ed6f995e78743dd1fa72c
miltonollie <miltonollie@d210e15f-bb6c-4fcc-83d8-0fb3a06c2c5b>
2016-03-18 12:51:58 +0000

    Added basic support for collections, arrays and maps.

--- test/ollie/utils/serialisation/JSONBuilderTest.java
+++ test/ollie/utils/serialisation/JSONBuilderTest.java
@@ -1,5 +1,10 @@
 package ollie.utils.serialisation;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -98,6 +103,37 @@ public class JSONBuilderTest {
 				.field("primNumberTest", 666L)
 				.field("objNumberTest", new Double(66.7))
 				.endObject()
+				.toString());
+	}
+	
+	@Test
+	public void arrayTest() {
+		Assert.assertEquals("{'array':['one','two','three']}", 
+				JSONBuilder.newSingleQuoteJSONBuilder()
+				.field("array", new String[]{"one","two","three"})
+				.toString());
+	}
+	
+	@Test
+	public void collectionTest() {
+		List<String> l = new ArrayList<>();
+		l.add("one");
+		l.add("two");
+		l.add("three");
+		Assert.assertEquals("{'collection':['one','two','three']}", 
+				JSONBuilder.newSingleQuoteJSONBuilder()
+				.field("collection", l)
+				.toString());
+	}
+	
+	@Test
+	public void mapTest() {
+		Map<String, String> map = new LinkedHashMap<>();
+		map.put("obj1", "one");
+		map.put("obj2", "two");
+		Assert.assertEquals("{'map':{'obj1':'one','obj2':'two'}}", 
+				JSONBuilder.newSingleQuoteJSONBuilder()
+				.field("map", map)
 				.toString());
 	}
 	
